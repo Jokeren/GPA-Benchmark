8b1d33e92fe0a8962b5c0f771982561f34504d8d
Jokeren <robinho364@gmail.com>
2020-07-15 21:12:56 -0500

    Refine first example

--- rodinia/backprop_opt/backprop_cuda_kernel.cu
+++ rodinia/backprop_opt/backprop_cuda_kernel.cu
@@ -8,7 +8,51 @@
 #include "math.h"
 #include "cuda.h"
 
+#define OPT1
 
+#ifdef OPT1
+__global__ void
+bpnn_layerforward_CUDA(float *input_cuda,
+	                   float *output_hidden_cuda,
+					   float *input_hidden_cuda,
+					   float *hidden_partial_sum,
+					   int in,
+					   int hid) 
+{
+   int by = blockIdx.y;
+   int tx = threadIdx.x;
+   int ty = threadIdx.y;
+
+   int index = ( hid + 1 ) * HEIGHT * by + ( hid + 1 ) * ty + tx + 1 + ( hid + 1 ) ;  
+
+   int index_in = HEIGHT * by + ty + 1;
+   
+   __shared__ float input_node[HEIGHT];
+   __shared__ float weight_matrix[HEIGHT][WIDTH];
+
+   weight_matrix[ty][tx] = input_hidden_cuda[index] * input_cuda[index_in];
+
+   __syncthreads();   
+   
+   for ( int i = 1 ; i <= __log2f(HEIGHT) ; i++){
+	   int power_two = __powf(2, i);
+
+	   if (ty % power_two == 0)
+       weight_matrix[ty][tx] = weight_matrix[ty][tx] + weight_matrix[ty + power_two/2][tx];
+
+	   __syncthreads();
+   }
+
+   input_hidden_cuda[index] = weight_matrix[ty][tx];
+
+   if (tx == 0) {
+	   hidden_partial_sum[by * hid + ty] = weight_matrix[tx][ty];
+   }
+
+}
+#endif
+
+#ifdef OPT2
 __global__ void
 bpnn_layerforward_CUDA(float *input_cuda,
 	                   float *output_hidden_cuda,
@@ -28,25 +72,9 @@ bpnn_layerforward_CUDA(float *input_cuda,
    __shared__ float input_node[HEIGHT];
    __shared__ float weight_matrix[HEIGHT][WIDTH];
 
-   if ( tx == 0 )
-     input_node[ty] = input_cuda[index_in] ;
-
-   __syncthreads();
-   
-   weight_matrix[ty][tx] = input_hidden_cuda[index] * input_node[ty];
+   weight_matrix[ty][tx] = input_hidden_cuda[index] * input_cuda[index_in];
 
    __syncthreads();   
-   
-   //for ( int i = 1 ; i <= __log2f(HEIGHT) ; i++){
- 
-	 //  int power_two = __powf(2, i);
-
-	 //  if( ty % power_two == 0 )
-	 //  weight_matrix[ty][tx] = weight_matrix[ty][tx] + weight_matrix[ty + power_two/2][tx];
-
-	 //  __syncthreads();
-
-   //}
    
    for ( unsigned int i = 2 ; i <= HEIGHT ; i *= 2){
      unsigned int power_two = i - 1;
@@ -65,6 +93,7 @@ bpnn_layerforward_CUDA(float *input_cuda,
    }
 
 }
+#endif
 
 
 __global__ void bpnn_adjust_weights_cuda(float * delta,   
