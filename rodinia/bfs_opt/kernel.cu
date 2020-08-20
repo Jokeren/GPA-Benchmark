baedbaa2e27464b0868fb6a83925225449d2115b
Jokeren <robinho364@gmail.com>
2020-08-19 21:58:47 -0500

    Add another opt

--- rodinia/bfs_opt/kernel.cu
+++ rodinia/bfs_opt/kernel.cu
@@ -26,9 +26,10 @@ Kernel( Node* g_graph_nodes, int* g_graph_edges, bool* g_graph_mask, bool* g_upd
   {
     g_graph_mask[tid]=false;
     int nedges = g_graph_nodes[tid].no_of_edges;
+    int starting = g_graph_nodes[tid].starting;
     int left = nedges - nedges / 4 * 4;
     int cost = g_cost[tid];
-    for (int i=g_graph_nodes[tid].starting; i<(left + g_graph_nodes[tid].starting); i+=1)
+    for (int i=starting; i<(left + starting); i+=1)
     {   
       int id1 = g_graph_edges[i];
       if(!g_graph_visited[id1])
@@ -37,7 +38,7 @@ Kernel( Node* g_graph_nodes, int* g_graph_edges, bool* g_graph_mask, bool* g_upd
         g_updating_graph_mask[id1]=true;
       }   
     }   
-    for (int i=g_graph_nodes[tid].starting + left; i<(nedges + g_graph_nodes[tid].starting); i+=4)
+    for (int i=starting + left; i<(nedges + starting); i+=4)
     {   
       int id1 = g_graph_edges[i];
       int id2 = g_graph_edges[i + 1]; 
